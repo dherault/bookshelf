49a4e51dbea16c1c17d004274db25d3fb04fd4a9
David Hérault <dherault@gmail.com>
2018-05-22 16:27:28 +0200

    design

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -13,6 +13,8 @@ const options = {
   keys: ['title', 'author'],
 };
 
+const apiUrl = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000';
+
 class App extends Component {
 
   constructor() {
@@ -26,7 +28,7 @@ class App extends Component {
   }
 
   componentWillMount() {
-    fetch('/books', { method: 'GET' })
+    fetch(apiUrl + '/books', { method: 'GET' })
     .then(response => response.json())
     .then(books => this.setState({ books }));
   }
@@ -36,7 +38,7 @@ class App extends Component {
   }
 
   handleDownloadClick = fileName => {
-    this.frameRef.current.src = `/download/${fileName}`;
+    this.frameRef.current.src = apiUrl + `/download/${fileName}`;
     // fetch(`http://localhost:3001/download/${fileName}`, { method: 'GET' });
   }
 
@@ -109,18 +111,10 @@ class App extends Component {
         <div className="books">
           {filteredBooks.map(book => (
             <div className="book" key={book.id}>
-              <div>
-                <div className="title">{book.title}</div>
-                <div className="author">{book.author}</div>
-              </div>
-              <div className="book-right">
-                <div className="language">{book.language}</div>
-                <div className="download" onClick={() => this.handleDownloadClick(book.fileName)}>
-                  <svg viewBox="0 0 512 512" width="20px">
-                    <path d="M382.56,233.376C379.968,227.648,374.272,224,368,224h-64V16c0-8.832-7.168-16-16-16h-64c-8.832,0-16,7.168-16,16v208h-64c-6.272,0-11.968,3.68-14.56,9.376c-2.624,5.728-1.6,12.416,2.528,17.152l112,128c3.04,3.488,7.424,5.472,12.032,5.472c4.608,0,8.992-2.016,12.032-5.472l112-128C384.192,245.824,385.152,239.104,382.56,233.376z" />
-                    <path d="M432,352v96H80v-96H16v128c0,17.696,14.336,32,32,32h416c17.696,0,32-14.304,32-32V352H432z" />
-                  </svg>
-                </div>
+              <div className="title" onClick={() => this.handleDownloadClick(book.fileName)}>{book.title}</div>
+              <div className="book-meta">
+                <span className="author">{book.author}</span>
+                <span className="language">{book.language}</span>
               </div>
             </div>
           ))}
